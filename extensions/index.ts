f41bcb18b7fefaf4f438343f1c5c43028c98df8d
StartupBros <will@startupbros.com>
2026-05-14 16:45:07 -0400

    chore(pi): migrate to earendil package scope

--- extensions/index.ts
+++ extensions/index.ts
@@ -1,5 +1,5 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { Type } from "@sinclair/typebox";
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import { Type } from "typebox";
 import { spawn } from "node:child_process";
 import fs from "node:fs/promises";
 import {
